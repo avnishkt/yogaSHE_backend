db0ae774d40091a999971efe9212b42ad728cda6
avnish <avnishkt16@gmail.com>
2023-05-15 17:38:01 +0530

    commited

--- model/candidateModel.js
+++ model/candidateModel.js
@@ -1,47 +1,48 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-const validator=require('email-validator')
-const validateEmail = function(v) {
-    return /\S+@akgec\.ac\.in$/.test(v);
-  };
-const candidateSchema= new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please Enter Your Name"],
-        maxLength: [30, "Name cannot exceed 30 characters"],
-        minLength: [4, "Name should have more than 4 characters"],
+const candidateSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter Your Name"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+    minLength: [4, "Name should have more than 4 characters"],
+  },
+  contact: {
+    type: Number,
+    required: true,
+    unique: true,
+    length: [10, "Enter the correct number"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return validator.isEmail(v);
       },
-    contact:{
-        type:Number,
-        required:true,
-        unique:true,
-        length:[10, "enter the correct number"]
+      message: 'Email address should be in a valid format',
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        validate: [validateEmail, 'Email address should be in the format user@akgec.ac.in'],
-    },
-    gender:{
-        type:String,
-        required:true,
+  },
+  gender: {
+    type: String,
+    // required: true,
+  },
+  branch: {
+    type: String,
+    // required: true,
+  },
+  studentNo: {
+    type: Number,
+    // required: true,
+    // unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
 
-    },
-    branch:{
-        type:String,
-        required:true,
-    },
-    studentNo:{
-        type:Number,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-
-    }
-})
-const Register=new mongoose.model('Candidate',candidateSchema)
-module.exports= Register;
+const Register = mongoose.model('Candidate', candidateSchema);
+module.exports = Register;
